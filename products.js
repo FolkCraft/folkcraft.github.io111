200e13541c8df83f81939e7904edf364e9118062
FolkCraft <56820164+FolkCraft@users.noreply.github.com>
2019-10-22 16:32:35 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,12 @@
+var uncukul = [
+    {"ID": "u01", "type": "Унцукульские мастера", "name": "23. Ваза декоративная", "path": "images/samples/uncukul/23.jpg", "description": "Высота - 110 мм"},
+    {"ID": "u02", "type": "Унцукульские мастера", "name": "24. Ваза декоративная", "path": "images/samples/uncukul/24.jpg", "description": "Высота - 110 мм"},
+    {"ID": "u03", "type": "Унцукульские мастера", "name": "25. Ваза декоративная", "path": "images/samples/uncukul/25.jpg", "description": "Высота - 110 мм"},
+    {"ID": "u04", "type": "Унцукульские мастера", "name": "26. Ваза декоративная", "path": "images/samples/uncukul/26.jpg", "description": "Высота - 110 мм"},
+    {"ID": "u05", "type": "Унцукульские мастера", "name": "47. Ваза декоративная", "path": "images/samples/uncukul/47.jpg", "description": "Высота - 110 мм"},
+    {"ID": "u06", "type": "Унцукульские мастера", "name": "23. Ваза декоративная", "path": "images/samples/uncukul/48.jpg", "description": "Высота - 110 мм"},
+]
+
+var kubachin = [
+
+]
